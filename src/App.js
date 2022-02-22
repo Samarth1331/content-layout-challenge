a53fc203803fac0c5ae848721a915079d3d022d6
Samarth1331 <77462226+Samarth1331@users.noreply.github.com>
2022-02-22 11:28:28 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './routes/Home'
-import ContentLayoutChallenge from './routes/ContentLayoutChallenge'
+import TodoList from './routes/TodoList'
 import './App.scss'
 
 export default function App() {
   return (
-    <div className='app'>
-      <Router>
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route path='/content-layout' component={ContentLayoutChallenge} />
-        </Switch>
-      </Router>
+    <div className='app'>      
+      <div>
+        <TodoList></TodoList>
+      </div>
     </div>
+
   )
 }
